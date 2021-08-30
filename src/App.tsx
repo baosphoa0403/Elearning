31e66c45fac62a24e756f79f9b12b711347f4b98
baosphoa0403 <baosphoa0403@gmail.com>
2021-08-30 21:20:19 +0700

    config rule

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const callAPI = () => {
+    setTimeout(() => {
+      alert('hello');
+    }, 500);
+
+    // console.log(abc1);
+  };
+  const abc = { name: 'bao', age: 'abc' };
+  const abc1 = 3;
+  //   var myFavoriteColor = '#112C85';
+  const hello = async (hello1: string) => {
+    if (hello1 === 'abc') {
+    }
+    const x = await callAPI();
+    return x;
+  };
+
+  // const array = [1, 3,]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        {abc.name}
+        <p></p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
+        <button
+          type='submit'
+          className='btn btn-danger'
+          onClick={() => {
+            alert('hello world');
+          }}>
+          Click
+        </button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
